d0ef8b4e1e1e6b3d6cdf793c0140f62e638e1c42
nannany <ymym1990ymym@gmail.com>
2018-12-30 19:16:57 +0900

    not foundまで

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { environment } from '../environments/environment'; 
-import { AngularFireModule } from '@angular/fire'; 
-import { AngularFirestoreModule } from '@angular/fire/firestore'; 
-import { AngularFireAuthModule } from '@angular/fire/auth'; 
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,21 +11,30 @@ import { PaginationModule } from 'ngx-bootstrap';
 import { ChatComponent } from './chat/chat.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './error/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: '', component: ChatComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase), 
-    AngularFirestoreModule,  
-    AngularFireAuthModule,  
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
     PaginationModule.forRoot(),
     SharedModule,
-    CoreModule
+    CoreModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
