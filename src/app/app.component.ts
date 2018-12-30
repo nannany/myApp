b907c718443bc7e55b7a777ebbcf5332792b2487
nannany <ymym1990ymym@gmail.com>
2018-12-30 18:43:19 +0900

    sharedまで

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,11 @@
 import { Component } from '@angular/core';
-import { Comment } from './class/comment';
-
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<app-chat></app-chat>',
 })
 export class AppComponent {
-  title = '掲示板';
-  count = 0;
-  name = '???'
-  message = '';
-  start = 0;
-  len = 10;
-  currentPage = 1;
-  comments: Observable<Comment[]>;
 
-  constructor(private db: AngularFirestore) {
-    this.comments = db.collection<Comment>('contents', ref => {
-      return ref.orderBy('date', 'asc')
-    })
-      .snapshotChanges()
-      .pipe(
-        map(actions => actions.map(action => {
-          const data = action.payload.doc.data() as Comment;
-          const comment_data = new Comment(data.name, data.message);
-          comment_data.setDate(data.date);
-          return comment_data;
-        })));
-  }
-
-  selector() {
-    this.count = this.message.length;
-  }
-
-  addComment(e: Event, message: string) {
-    e.preventDefault();
-    if (message) {
-      this.db
-        .collection('contents')
-        .add(new Comment(this.name, message).deserialize());
-      this.message = '';
-    }
-  }
-
-  pager(event: any): void {
-    this.start = this.len * (event.page - 1);
+  constructor() {
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { environment } from '../environments/environment'; 
 import { AngularFireModule } from '@angular/fire'; 
 import { AngularFirestoreModule } from '@angular/fire/firestore'; 
@@ -8,22 +7,23 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Nl2brPipe } from './nl2br.pipe';
-import {PaginationModule} from 'ngx-bootstrap';
+import { PaginationModule } from 'ngx-bootstrap';
+import { ChatComponent } from './chat/chat.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Nl2brPipe
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     AngularFireModule.initializeApp(environment.firebase), 
     AngularFirestoreModule,  
     AngularFireAuthModule,  
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
